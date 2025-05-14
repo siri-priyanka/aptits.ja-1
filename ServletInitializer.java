11922bb4f3681984b76965d246e2a525c2b17e39
fareed674 <fareedshareef679@gmail.com>
2025-05-14 15:39:23 +0530

    Update ServletInitializer.java

--- ServletInitializer.java
+++ ServletInitializer.java
@@ -1,12 +1,1 @@
-package com.example;
 
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-public class ServletInitializer extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(AptItSolutionsApplication.class); // Replace with your main class if named differently
-    }
-}
