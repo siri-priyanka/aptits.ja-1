dda16baff108b52c37a77c4167c8524b658d7809
fareed674 <fareedshareef679@gmail.com>
2025-05-14 15:36:14 +0530

    Update AptItSolutionsApplication.java

--- src/main/java/com/example/AptItSolutions/AptItSolutionsApplication.java
+++ src/main/java/com/example/AptItSolutions/AptItSolutionsApplication.java
@@ -1,33 +1,17 @@
-// package com.example.AptItSolutions;
+package com.example.AptItSolutions;
 
-// import org.springframework.boot.SpringApplication;
-// import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-// @SpringBootApplication
-// public class AptItSolutionsApplication {
+@SpringBootApplication
+public class AptItSolutionsApplication {
 
-// 	public static void main(String[] args) {
-// 		SpringApplication.run(AptItSolutionsApplication.class, args);
+	public static void main(String[] args) {
+		SpringApplication.run(AptItSolutionsApplication.class, args);
 
-// 		System.out.println("HELLO APT IT SOLUTIONS");
-package com.example; // Use the same package as your main application
-
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-public class ServletInitializer extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(AptItSolutionsApplication.class); // Your main class name
-    }
-}
+		System.out.println("HELLO APT IT SOLUTIONS");
 	
-		
-		
-		
-		
-// 	}
+	}
 
-// }
+}
